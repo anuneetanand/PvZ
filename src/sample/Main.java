db0ef0e1bd64982db517f135c10bd67a9f6b50c6
Anuneet Anand <anuneet18022@iiitd.ac.in>
2019-10-30 21:06:52 +0530

    lol

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,21 +13,60 @@ import javafx.scene.input.KeyEvent;
 public class Main extends Application
 {
     private static Stage stage;
+    private Parent root_Main_Menu;
+    private Parent root_Game;
+    private Parent root_PauseMenu;
+    private Parent root_LeaderBoard;
+    private Parent root_Help;
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         stage = primaryStage;
         Parent root_Main_Menu = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
-        //Parent root_Game = FXMLLoader.load(getClass().getResource("GAME.fxml"));
-        //Parent root_PauseMenu = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
-        //Parent root_LeaderBoard = FXMLLoader.load(getClass().getResource("LeaderBoard.fxml"));
-        //Parent root_Help = FXMLLoader.load(getClass().getResource("Help.fxml"));
+        Parent root_Game = FXMLLoader.load(getClass().getResource("GAME.fxml"));
+        Parent root_PauseMenu = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
+        Parent root_LeaderBoard = FXMLLoader.load(getClass().getResource("LeaderBoard.fxml"));
+        Parent root_Help = FXMLLoader.load(getClass().getResource("Help.fxml"));
         stage.setScene(new Scene(root_Main_Menu));
-        stage.addEventHandler(MouseEvent.MOUSE_CLICKED , (MouseEvent event) -> {System.out.println("Clicked");});
         stage.setFullScreen(true);
         stage.show();
         stage.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {if (KeyCode.ESCAPE == event.getCode()) {primaryStage.close();}});
     }
+
+    public Parent getRoot_Main_Menu() { return root_Main_Menu; }
+    public void setRoot_Main_Menu(Parent root_Main_Menu) { this.root_Main_Menu = root_Main_Menu; }
+    public Parent getRoot_Game() {
+        return root_Game;
+    }
+
+    public void setRoot_Game(Parent root_Game) {
+        this.root_Game = root_Game;
+    }
+
+    public Parent getRoot_PauseMenu() {
+        return root_PauseMenu;
+    }
+
+    public void setRoot_PauseMenu(Parent root_PauseMenu) {
+        this.root_PauseMenu = root_PauseMenu;
+    }
+
+    public Parent getRoot_LeaderBoard() {
+        return root_LeaderBoard;
+    }
+
+    public void setRoot_LeaderBoard(Parent root_LeaderBoard) {
+        this.root_LeaderBoard = root_LeaderBoard;
+    }
+
+    public Parent getRoot_Help() {
+        return root_Help;
+    }
+
+    public void setRoot_Help(Parent root_Help) {
+        this.root_Help = root_Help;
+    }
+
     public static Stage getStage() { return stage; }
     public static void setStage(Stage stage) { Main.stage = stage; }
     public static void main(String[] args) { launch(args); }
--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.MediaPlayer;
 
 public class MainMenuController extends Click
@@ -8,6 +10,19 @@ public class MainMenuController extends Click
     private static MediaPlayer Music;
 
     @FXML
+    public void New
     public void Exit(javafx.scene.input.MouseEvent mouseEvent)
     { System.exit(0); }
+
+    public void NewGame(MouseEvent mouseEvent)
+    { Main.getStage().setScene(new Scene()); }
+
+    public void Help(MouseEvent mouseEvent)
+    { }
+
+    public void LeaderBoard(MouseEvent mouseEvent)
+    { }
+
+    public void LoadGame(MouseEvent mouseEvent)
+    { }
 }
