4be7474cd0c88d1c4367a73b3a9f973e203fbf98
Anuneet Anand <anuneet18022@iiitd.ac.in>
2019-10-30 23:44:26 +0530

    ok

--- src/sample/LeaderBoardController.java
+++ src/sample/LeaderBoardController.java
@@ -1,11 +1,12 @@
 package sample;
 
 import javafx.scene.input.MouseEvent;
+import java.io.IOException;
 
-public class LeaderBoardController
+public class LeaderBoardController extends Click
 {
-    public void Back(MouseEvent mouseEvent)
+    public void Back(MouseEvent mouseEvent) throws IOException
     {
-
+        Main.setRoot_Main_Menu();
     }
 }
