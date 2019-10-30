a0cee42b354f11cdb9256ebdd14751787268b2de
Anuneet Anand <anuneet18022@iiitd.ac.in>
2019-10-31 00:26:24 +0530

    ok

--- src/sample/PauseMenuController.java
+++ src/sample/PauseMenuController.java
@@ -1,4 +1,17 @@
 package sample;
 
-public class PauseMenuController
-{ }
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+public class PauseMenuController extends Click
+{
+    public void Music(MouseEvent mouseEvent)
+    { }
+
+    public void RestartGame(MouseEvent mouseEvent) throws IOException { Main.setRoot_Game();}
+
+    public void SaveGame(MouseEvent mouseEvent) throws IOException { Main.setRoot_Main_Menu(); }
+
+    public void Back(MouseEvent mouseEvent) throws IOException { Main.setRoot_Game(); }
+}
