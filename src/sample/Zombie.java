1ab14da8a84b054520e71b1e9a9ed3ca954b4825
Anuneet Anand <anuneet18022@iiitd.ac.in>
2019-11-19 17:55:09 +0530

    GUI Done

--- src/sample/Zombie.java
+++ src/sample/Zombie.java
@@ -1,7 +1,14 @@
 package sample;
 
-public class Zombie
+public class Zombie extends Character
 {
-    private int Danage;
+    private int Damage;
     private int Row;
+    private String Type;
+    public Zombie(int x, int y, String type, int r , int d) { super(x, y); Type=type; Damage = d; Row = r; }
+    public void Eat(){}
+    public String getType() { return Type; }
+    public void setType(String type) { Type = type; }
+    public int getDamage() { return Damage; }
+    public int getRow() { return Row; }
 }
